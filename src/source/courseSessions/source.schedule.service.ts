10bac1c671658c30283283104dde6b270dc1b4ff
m4s1t4 <jmrodriguezm13@gmail.com>
2025-02-05 21:21:19 -0300

    Course Session function woking

--- src/source/courseSessions/source.schedule.service.ts
+++ src/source/courseSessions/source.schedule.service.ts
@@ -9,26 +9,7 @@ export class SourceScheduleService {
   }
   async getCourseSessionsBySubject(subjectName: string) {
     const sessions = await this.parserStrategy.getCourseSessionsBySubject(subjectName);
-    const session = {
-      subject: {
-        name: "Sistemas Operativos",
-        year: "3",
-        term: "1",
-        courseCode: "SO",
-        deparment: {
-          name: "Sistemas de Información"
-        }
-      },
-      professor: "",
-      classroom: {
-        building: "Sistemas",
-        floor: "Planta Baja",
-        classroom: 5
-      },
-      schdule: [{ day: "2025-02 - 12", startTime: null, endTime: null }],
-      curriculum: "Ingenieria en Sistemas"
-    };
-    console.log(session)
-    return session
+    console.log(sessions)
+    return sessions
   }
 }
