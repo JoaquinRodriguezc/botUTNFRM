351b00da2c6c514d46eebf651283cd07e496482c
m4s1t4 <jmrodriguezm13@gmail.com>
2025-02-09 23:10:27 -0300

    Abbreviations add to systemprompt

--- src/source/subjects/source.subjects.service.ts
+++ src/source/subjects/source.subjects.service.ts
@@ -1,8 +1,12 @@
+import * as abbreviations from './abbreviations.json';
 import * as subjects from './subjects.json';
 export class SourceSubjectsService {
   constructor() {}
 
   async getSubjects() {
     return subjects;
+  }
+  async getabreviations() {
+    return abbreviations;
   }
 }
