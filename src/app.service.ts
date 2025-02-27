4fe90f84145faf4716c6dc386279922a3c0acf50
Joaquin Rodriguez <rodriguezcjoaquin02@gmail.com>
2025-02-27 00:21:20 -0300

    add cron to delete conversations

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
-
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cache } from 'cache-manager';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+  @Cron(CronExpression.EVERY_10_MINUTES)
+  async clearCache() {
+    await this.cacheManager.clear();
   }
 }
